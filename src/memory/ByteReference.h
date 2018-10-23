1e13a1148aea695001101ca5dc577bbbe64a42ab
John Moses <john.moses445@gmail.com>
2018-10-22 20:42:53 -0700

    Cleanup ByteReference

--- src/memory/ByteReference.h
+++ src/memory/ByteReference.h
@@ -12,11 +12,8 @@ public:
     ~ByteReference() override = default;
 
     auto read8() -> uint8_t override;
-    auto read16() -> uint16_t override { return 1; }
 
     auto write8(uint8_t value) -> void override;
-    auto write16(uint16_t valie) -> void override { ; }
-
 private:
     uint8_t& m_value;
 };
